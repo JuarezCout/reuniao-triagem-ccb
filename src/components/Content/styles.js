d81542037844fb5fc1a871ac96171ca38f321cb8
JuarezCout <juarezcout15.jl@gmail.com>
2021-10-05 13:33:11 -0300

    feat: added main content page

--- src/components/Content/styles.js
+++ src/components/Content/styles.js
@@ -2,4 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: CT;
+
+  .table{
+    tbody{
+      border-spacing: 20px;
+      border-collapse: separate;
+
+      td {
+        border-bottom-width: 30px;
+        background: #d3d3d3;
+        border-color: #ffff;
+      }
+    }
+  }
+
+    
 `;
